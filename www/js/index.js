0252a5e2a4b34519bfe79db6cd051dc6e6a66ecb
Tred1998 <43576052+Tred1998@users.noreply.github.com>
2018-10-09 15:20:17 +0100

    Update index.js
    change the pause and play with blue and red

--- www/js/index.js
+++ www/js/index.js
@@ -31,7 +31,7 @@ function updateDisplay() {
 	paused_count++;
 	updateDisplay();
 	    
-	alert("pause");
+	alert("blue");
     }
 	
 
@@ -40,5 +40,5 @@ function updateDisplay() {
 	resumed_count++;
 	updateDisplay();
 	    
-	alert("resume");
+	alert("red");
     }
